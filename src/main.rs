5a2372e18c3fbe68fb1d6e1ca7fce0871ce3bda1
Russell Hay <me@russellhay.com>
2017-02-20 09:35:18 -0800

    Adding serialization and begining to refactor to allow multiple files to be processed

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod operation;
 mod tokenize;
 mod log_entry;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 extern crate chrono;
 extern crate clap;
 
@@ -13,35 +16,65 @@ use std::path::Path;
 use std::fs::File;
 use std::io::BufReader;
 use std::collections::HashMap;
+use chrono::{DateTime, UTC};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OutputEntry {
+    unique_id : String,
+    timestamp : DateTime<UTC>,
+    episode_number: u16,
+    complete_download: bool
+}
+
+impl OutputEntry {
+    fn from(e : &LogEntry) -> OutputEntry {
+        OutputEntry {
+            unique_id: match e.request_id {
+                Some(ref x) => x.clone(),
+                None => "".to_string()
+            },
+            timestamp: e.timestamp.clone(),
+            episode_number: 0,
+            complete_download: e.was_complete_download()
+        }
+    }
+}
 
 fn main() {
     let matches = App::new("Podlog")
         .version("0.1")
         .arg(Arg::with_name("FILENAME")
-                .required(true)
-                .index(1))
-    .get_matches();
+            .required(true)
+            .index(1))
+        .get_matches();
 
-    let path = Path::new(matches.value_of("FILENAME").unwrap());
+    let filename = matches.value_of("FILENAME").unwrap();
+    foo(filename);
+}
+
+fn foo(filename : &str) -> () {
+
+    let path = Path::new(filename);
     let file = BufReader::new(File::open(&path).unwrap());
     let lines = file.lines().filter_map(|result| result.ok()); // Filter out bad rows
     let entries = lines.map(|x| LogEntry::from_str(&x))
                        .filter(|x| x.operation.source == OperationSource::WEBSITE)
                        .filter(|ref x| x.is_audio_file());
 
-    let mut results : HashMap<String, Vec<&str> > = HashMap::new();
+    let mut results : HashMap<String, Vec<OutputEntry> > = HashMap::new();
     for entry in entries {
         let path = entry.path.clone().unwrap();
         if !results.contains_key(&path) {
             results.insert(path, Vec::new());
         } else {
-            match results.get_mut(&path) {
-                Some(x) => x.push("foo"),
-                None => ()
+            let mut x = results.get_mut(&path);
+            if x.is_some() {
+                x.as_mut().unwrap().push(OutputEntry::from(&entry));
             }
         }
     }
-    println!("{:?}", results);
+    let storage = serde_json::to_string_pretty(&results).unwrap();
+    println!("{}", storage);
 }
 
 // Output Format:
