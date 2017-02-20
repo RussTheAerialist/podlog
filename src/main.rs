171c32f40c357dff4c69f3e8e7ab72d8d679651d
Russell Hay <me@russellhay.com>
2017-02-19 22:05:34 -0800

    Entries are being split properly

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ extern crate chrono;
 extern crate clap;
 
 use log_entry::log_entry::LogEntry;
+use operation::operation::OperationSource;
 use clap::{Arg, App};
 use std::io::prelude::*;
 use std::path::Path;
 use std::fs::File;
 use std::io::BufReader;
+use std::collections::HashMap;
 
 fn main() {
     let matches = App::new("Podlog")
@@ -23,7 +25,24 @@ fn main() {
     let path = Path::new(matches.value_of("FILENAME").unwrap());
     let file = BufReader::new(File::open(&path).unwrap());
     let lines = file.lines().filter_map(|result| result.ok()); // Filter out bad rows
-    let entries = lines.map(|x| LogEntry::from_str(&x)); // Turn them into log entries
+    let entries = lines.map(|x| LogEntry::from_str(&x))
+                       .filter(|x| x.operation.source == OperationSource::WEBSITE)
+                       .filter(|ref x| x.is_audio_file());
 
-    // Create a hashmap based on path name
+    let mut results : HashMap<String, Vec<&str> > = HashMap::new();
+    for entry in entries {
+        let path = entry.path.clone().unwrap();
+        if !results.contains_key(&path) {
+            results.insert(path, Vec::new());
+        } else {
+            match results.get_mut(&path) {
+                Some(x) => x.push("foo"),
+                None => ()
+            }
+        }
+    }
+    println!("{:?}", results);
 }
+
+// Output Format:
+// UniqueID, DateTime, Episode, Was Complete Download
